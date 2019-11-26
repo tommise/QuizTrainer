e2b87fd407a041a23e2a2279c9b31a160c434c22
Tommi Ek <tommiek83@gmail.com>
2019-11-26 14:36:46 +0200

    Moved project main to ui.UserInterface

--- QuizTrainer/src/main/java/quiztrainer/Main.java
+++ QuizTrainer/src/main/java/quiztrainer/Main.java
@@ -1,15 +1,10 @@
+package quiztrainer;
 
-import java.util.*;
-import logic.Application;
-import ui.UserInterface;
+import quiztrainer.ui.UserInterface;
 
 public class Main {
         
     public static void main(String[] args) {
-        Application app = new Application();
-        Scanner scanner = new Scanner(System.in);
-            
-        UserInterface ui = new UserInterface(app, scanner);
-        ui.start();
+        UserInterface.main(args);
     }
 }
