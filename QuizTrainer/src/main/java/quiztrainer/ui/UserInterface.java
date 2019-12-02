13880a402cb6f71324422b4a54fd6b1342566d37
Tommi Ek <tommiek83@gmail.com>
2019-12-03 00:07:43 +0200

    Added login, signup and starting scenes

--- QuizTrainer/src/main/java/quiztrainer/ui/UserInterface.java
+++ QuizTrainer/src/main/java/quiztrainer/ui/UserInterface.java
@@ -2,8 +2,9 @@
 package quiztrainer.ui;
 
 import java.util.*;
+import quiztrainer.domain.Deck;
 import quiztrainer.domain.QuizCard;
-import quiztrainer.logic.Trainer;
+import quiztrainer.domain.QuizTrainerService;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -17,35 +18,147 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class UserInterface extends Application {
+    private QuizTrainerService trainer;
+    
+    private Scene startingScene;
+    private Scene loginScene;
+    private Scene signupScene;    
+    private Scene mainScene;
     private Scene rehearseScene;
     private Scene addANewCardScene;
-    private Scene mainScene;
+    private Label currentUserLabel = new Label("");  
     
     @Override
     public void init() throws Exception {
+        this.trainer = new QuizTrainerService();
     }
     
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Trainer trainer = new Trainer();
         GUIHelper guiHelper = new GUIHelper();
+        Deck deck = new Deck("Default deck");
+        
+        // Starting scene
+        
+        VBox startingPane = new VBox(20);
+        startingPane.setPadding(new Insets(20));
+        startingPane.setPadding(new Insets(20));        
+        Label welcomeLabel = new Label("Welcome to QuizTrainer!"); 
+        Button loginStartSceneButton = new Button("Login");
+        Button signupStartSceneButton = new Button("Signup");
+        
+        startingPane.getChildren().addAll(welcomeLabel, loginStartSceneButton, signupStartSceneButton); 
+        startingScene = new Scene(startingPane, 500, 500); 
+        
+        loginStartSceneButton.setOnAction(e-> {
+            primaryStage.setScene(loginScene);
+        });
+
+        signupStartSceneButton.setOnAction(e-> {
+            primaryStage.setScene(signupScene);
+        });        
+        
+        // Login scene
+        
+        VBox loginPane = new VBox(20); 
+        loginPane.setPadding(new Insets(20));
+        loginPane.setPadding(new Insets(20));         
+        Label usernameInstructionLabel = new Label("Login with your username:") ;   
+        TextField usernameInput = new TextField();        
+        Button loginButton = new Button("Login");
+        Button returnFromLoginButton = new Button("<- Return to menu"); 
+        Label loginWarning = new Label("");
+        
+        loginPane.getChildren().addAll(usernameInstructionLabel, usernameInput, loginButton, returnFromLoginButton, loginWarning); 
+        loginScene = new Scene(loginPane, 500, 500);
+        
+        loginButton.setOnAction(e-> {
+            String username = usernameInput.getText();
+            if (trainer.login(username)){
+                usernameInput.setText("");
+                loginWarning.setText("");
+                currentUserLabel.setText("Welcome " + trainer.getCurrentUser().getName() + "!");
+                primaryStage.setScene(mainScene);  
+            } else {
+                loginWarning.setText("User does not exist.");
+                loginWarning.setTextFill(Color.RED);                
+            }
+        });
+        
+        returnFromLoginButton.setOnAction(e-> {
+            usernameInput.setText("");
+            loginWarning.setText("");            
+            primaryStage.setScene(startingScene);
+        });        
+        
+        // Signup scene
+        
+        VBox signupPane = new VBox(20);
+        signupPane.setPadding(new Insets(20));
+        signupPane.setPadding(new Insets(20)); 
+        Label newUserInstruction = new Label("Choose a username:");   
+        TextField newUsernameInput = new TextField(); 
+        Label newNameInstruction = new Label("Please fill in your name:");         
+        TextField newNameInput = new TextField(); 
+        Button signupButton = new Button("Signup"); 
+        Button returnFromSignupButton = new Button("<- Return to menu"); 
+        Label signupMessage = new Label("");
+ 
+        signupPane.getChildren().addAll(newUserInstruction, newUsernameInput, newNameInstruction, newNameInput, signupButton, signupMessage, returnFromSignupButton);
+        signupScene = new Scene(signupPane, 500, 500);
+        
+        signupButton.setOnAction(e-> {
+            String username = newUsernameInput.getText();
+            String name = newNameInput.getText();
+            
+            ArrayList<String> inputs = new ArrayList<>();
+            inputs.add(username);
+            inputs.add(name);
+            
+            if (!guiHelper.inputsAreValid(inputs)) {
+                signupMessage.setText("Username contains invalid characters or form is empty.");
+                signupMessage.setTextFill(Color.RED);
+            } else if (trainer.addANewUser(username, name)) {
+                signupMessage.setText("You have succesfully created an account with username: " + newUsernameInput.getText());
+                signupMessage.setTextFill(Color.BLACK);
+                newUsernameInput.setText("");
+                newNameInput.setText("");
+            } else {
+                signupMessage.setText("Username is already taken.");
+                signupMessage.setTextFill(Color.RED);
+            }
+        });
+            
+        returnFromSignupButton.setOnAction(e-> {
+            newUsernameInput.setText("");
+            newNameInput.setText("");            
+            signupMessage.setText("");  
+            primaryStage.setScene(startingScene);
+        });
         
         // Primary scene
+        
         VBox primaryPane = new VBox(20);
         primaryPane.setPadding(new Insets(20));
         primaryPane.setPadding(new Insets(20));
-        Label welcomeLabel = new Label("Welcome to QuizTrainer!");
         Button primaryAddANewCardButton = new Button("Add a new card");
         Button primaryRehearseButton = new Button("Rehearse");
+        Button logoutButton = new Button("Logout");
         Label errorLabel = new Label("");
-        primaryPane.getChildren().addAll(welcomeLabel, primaryAddANewCardButton, primaryRehearseButton, errorLabel);
-        
-        Button returnFromAddButton = new Button("<- Return to menu");        
-        
+        primaryPane.getChildren().addAll(currentUserLabel, primaryAddANewCardButton, primaryRehearseButton, errorLabel, logoutButton);
+
+        logoutButton.setOnAction(e-> {
+            trainer.logout();
+            currentUserLabel.setText("");
+            primaryStage.setScene(startingScene);
+        });
+                
         // Scene for adding a new card
+        
         VBox addANewCardPane = new VBox(20);
         addANewCardPane.setPadding(new Insets(20));
         addANewCardPane.setPadding(new Insets(20));
+        Button returnFromAddButton = new Button("<- Return to menu");        
         
         addANewCardScene = new Scene(addANewCardPane, 500, 500);
         
@@ -82,23 +195,20 @@ public class UserInterface extends Application {
         addANewCardButton.setOnAction(e-> {
             String question = newQuestionInput.getText();
             String answer = newAnswerInput.getText();
-            String choice1 = wrongChoice1Input.getText();
-            String choice2 = wrongChoice2Input.getText();
-            String choice3 = wrongChoice3Input.getText();
             ArrayList<String> choices = new ArrayList<>();
-            choices.add(choice1);
-            choices.add(choice2);
-            choices.add(choice3);
+            choices.add(wrongChoice1Input.getText());
+            choices.add(wrongChoice2Input.getText());
+            choices.add(wrongChoice3Input.getText());
             ArrayList<String> inputs = new ArrayList<>();
             inputs.add(question);
             inputs.add(answer);
-            inputs.add(choice1);
-            inputs.add(choice2);
-            inputs.add(choice3);       
+            inputs.add(wrongChoice1Input.getText());
+            inputs.add(wrongChoice2Input.getText());
+            inputs.add(wrongChoice3Input.getText());       
             
             if (guiHelper.inputsAreValid(inputs)) {
                 QuizCard newQuizCard = new QuizCard(question, answer, choices);
-                trainer.addCard(newQuizCard);
+                deck.addANewCard(newQuizCard);
                 newQuestionInput.setText("");
                 newAnswerInput.setText("");
                 wrongChoice1Input.setText("");
@@ -149,7 +259,7 @@ public class UserInterface extends Application {
         rehearsePane.getChildren().addAll(question, answerPane, resultLabel, nextQuestionButton, returnFromRehearseButton); 
         
         primaryRehearseButton.setOnAction(e-> {
-            QuizCard quizCard = trainer.getNextQuestion();
+            QuizCard quizCard = deck.getNextQuestion();
             if (quizCard == null) {
                 errorLabel.setText("There are no cards to rehearse.");
                 errorLabel.setTextFill(Color.RED);             
@@ -167,36 +277,36 @@ public class UserInterface extends Application {
         
         answer1Button.setOnAction(e-> {
             String answer = answer1Button.getText();
-            QuizCard quizCard = trainer.leitner.getACard(question.getText());
+            QuizCard quizCard = deck.getACard(question.getText());
             if (quizCard.isCorrectAnswer(answer)) {
-                resultLabel.setText(trainer.correctAnswer(quizCard));             
+                resultLabel.setText(trainer.correctAnswer(quizCard, deck));             
             } else {
-                resultLabel.setText(trainer.wrongAnswer(quizCard));
+                resultLabel.setText(trainer.wrongAnswer(quizCard, deck));
             }
         });
         
         answer2Button.setOnAction(e-> {
             String answer = answer2Button.getText();     
-            QuizCard quizCard = trainer.leitner.getACard(question.getText());
+            QuizCard quizCard = deck.getACard(question.getText());
             if (quizCard.isCorrectAnswer(answer)) {
-                resultLabel.setText(trainer.correctAnswer(quizCard));
+                resultLabel.setText(trainer.correctAnswer(quizCard, deck));
             } else {
-                resultLabel.setText(trainer.wrongAnswer(quizCard));
+                resultLabel.setText(trainer.wrongAnswer(quizCard, deck));
             } 
         });
 
         answer3Button.setOnAction(e-> {
             String answer = answer3Button.getText();  
-            QuizCard quizCard = trainer.leitner.getACard(question.getText());            
+            QuizCard quizCard = deck.getACard(question.getText());            
             if (quizCard.isCorrectAnswer(answer)) {
-                resultLabel.setText(trainer.correctAnswer(quizCard));
+                resultLabel.setText(trainer.correctAnswer(quizCard, deck));
             } else {
-                resultLabel.setText(trainer.wrongAnswer(quizCard));
+                resultLabel.setText(trainer.wrongAnswer(quizCard, deck));
             }  
         });        
         
         nextQuestionButton.setOnAction(e-> {
-            QuizCard nextCard = trainer.getNextQuestion();
+            QuizCard nextCard = deck.getNextQuestion();
             question.setText(nextCard.getQuestion());
             ArrayList<String> nextCardChoices = nextCard.generateChoices();
             answer1Button.setText(nextCardChoices.get(0));
@@ -212,7 +322,7 @@ public class UserInterface extends Application {
         mainScene = new Scene(primaryPane, 500, 500);
         
         primaryStage.setTitle("QuizTrainer");
-        primaryStage.setScene(mainScene);
+        primaryStage.setScene(startingScene);
         primaryStage.show();
     }
     
