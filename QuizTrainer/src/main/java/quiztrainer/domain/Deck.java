e175873976679d4ed7f92d45031f5b5f3e099dbb
Tommi Ek <tommiek83@gmail.com>
2019-12-03 00:00:02 +0200

    New Deck class and JUnit tests, separated from earlier Leitner for better project logic

---
+++ QuizTrainer/src/main/java/quiztrainer/domain/Deck.java
@@ -0,0 +1,103 @@
+
+package quiztrainer.domain;
+
+import java.util.*;
+import quiztrainer.logic.CardInterval;
+import quiztrainer.logic.Leitner;
+
+public class Deck {
+    
+    ArrayList<Box> boxes;
+    Box box1, box2, box3, box4, box5;
+    Leitner leitner;
+    CardInterval interval;
+    String deckName;
+    
+    public Deck(String deckName) {
+        this.deckName = deckName;
+        this.leitner = new Leitner();
+        this.interval = new CardInterval();
+        
+        this.boxes = new ArrayList<>();
+        
+        this.box1 = new Box(1);
+        this.box2 = new Box(2);
+        this.box3 = new Box(3);
+        this.box4 = new Box(4);
+        this.box5 = new Box(5);
+        
+        this.boxes.add(box1);
+        this.boxes.add(box2);
+        this.boxes.add(box3);
+        this.boxes.add(box4);
+        this.boxes.add(box5);  
+    }
+    
+    public void addANewCard(QuizCard card) {
+        this.box1.addACard(card);
+    }
+    
+    public void addACard(QuizCard card, int boxNumber) {
+        
+        for (Box box : boxes) {
+            if (box.getBoxNumber() == boxNumber) {
+                box.addACard(card);
+            }
+        }
+    }    
+    
+    public Box getCurrentBox(QuizCard card) {
+
+        for (Box box : this.boxes) {
+            if (box.getQuizCards().isEmpty()) {
+                continue;
+            }
+            
+            ArrayList<QuizCard> kortit = box.getQuizCards();
+            if (kortit.contains(card)) {
+                return box;
+            }
+        }
+        
+        return null;
+    }
+    
+    public QuizCard getACard(String question) {
+        
+        for (Box box : this.boxes) {
+            if (box.getQuizCards().isEmpty()) {
+                continue;
+            }
+            
+            ArrayList<QuizCard> kortit = box.getQuizCards();
+            for (QuizCard quizCard : kortit) {
+                if (quizCard.getQuestion().equals(question)) {
+                    return quizCard;
+                }
+            }
+        }
+        
+        return null;
+    }
+    
+    public QuizCard getNextQuestion() {
+        int totalSize = 0;
+            
+        for (Box box: boxes) {
+            int sizeOfBox = box.getQuizCards().size();
+            totalSize += sizeOfBox;
+        }
+        
+        if (totalSize == 0) {
+            return null;
+        }
+            
+        QuizCard nextCard = this.interval.drawANewCard(boxes);
+        
+        return nextCard;
+    } 
+        
+    public ArrayList<Box> getBoxes() {
+        return this.boxes;
+    }
+}
---
+++ QuizTrainer/src/test/java/quiztrainer/domain/DeckTest.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiztrainer.domain;
+
+import quiztrainer.logic.Leitner;
+
+
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class DeckTest {
+    
+    QuizCard quizCard;
+    Leitner leitner;
+    Deck deck;
+    ArrayList<Box> boxes;    
+    
+    @Before
+    public void setUp() {
+        String question = "What is the capital city of Finland?";
+        String answer = "Helsinki";
+        ArrayList<String> falseAnswers = new ArrayList<>();
+        falseAnswers.add("Tokyo");
+        falseAnswers.add("Oslo");
+        falseAnswers.add("Shanghai");
+        
+        quizCard = new QuizCard(question, answer, falseAnswers);
+        leitner = new Leitner();
+        deck = new Deck("Default deck");
+        boxes = deck.getBoxes();       
+    }
+    
+    @Test
+    public void allBoxesAreEmptyAtStart() {
+        int totalSize = 0;
+        
+        for (int i = 0; i < boxes.size(); i++) {
+            Box box = boxes.get(i);
+            totalSize += box.getQuizCards().size();
+            
+        }
+        assertEquals(0, totalSize);
+    }
+    
+    @Test
+    public void newCardGoesToBoxOne() {
+        Box box1 = boxes.get(0);
+        deck.addANewCard(quizCard);
+        assertEquals(1, box1.getQuizCards().size());
+    }
+    
+    @Test
+    public void addingACardToSpecificBoxGoesToRightBox() {
+        Box box5 = boxes.get(4);
+        deck.addACard(quizCard, 5);
+        assertEquals(1, box5.getQuizCards().size());
+    }
+    
+    @Test
+    public void searchingWithQuestionReturnsRightCard() {
+        deck.addANewCard(quizCard);
+        String question = "What is the capital city of Finland?";
+        
+        assertEquals(quizCard, deck.getACard(question));
+    } 
+    
+    @Test
+    public void searchingForABoxWithGivenCardReturnsRightBox() {
+        deck.addACard(quizCard, 4);
+        int boxNumber = deck.getCurrentBox(quizCard).getBoxNumber();
+        
+        assertEquals(4, boxNumber);
+    }
+}
